4586b5596381f14b44a2ffd78d359b1c5a94a35f
OlgaKuksa <firsty@tut.by>
2018-09-11 20:23:28 +0300

    add exchange solution

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    const coins=[{Key:"H", Value:50}, {Key:"Q", Value:25},{Key:"D", Value:10},{Key:"N", Value:5},{Key:"P", Value:1}];
+    if (currency<=0) return {};
+    if (currency>10000) return {error:"You are rich, my friend! We don't have so much coins for exchange"};
+    let ret={};
+    let leftovers=currency;
+    coins.forEach(coin => {
+        let count=Math.floor(leftovers/coin.Value);
+        if (count>=1)
+        {
+            ret[coin.Key]=count;
+            leftovers=leftovers%coin.Value;
+        }
+    });
+    return ret;
 }
